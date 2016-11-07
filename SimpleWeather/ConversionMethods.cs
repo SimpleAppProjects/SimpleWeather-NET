798d21cf3e3c1820d9f5a28da7363a2951d38ad3
Dave Antoine <bryan2894@gmail.com>
2016-11-07 01:34:42 -0500

    SimpleWeather: update ConversionMethods
    * Add functions to convert Farenheit to Celsius
    * Cleanup formatting and functions

--- SimpleWeather/ConversionMethods.cs
+++ SimpleWeather/ConversionMethods.cs
@@ -8,54 +8,30 @@ namespace SimpleWeather
 {
     public static class ConversionMethods
     {
-        public const double KM_TO_MI = 0.621371192;
-        public const double MI_TO_KM = 1.609344;
+        private const double KM_TO_MI = 0.621371192;
+        private const double MI_TO_KM = 1.609344;
 
         public static string mbToInHg(String input)
         {
             double result = 29.92 * double.Parse(input) / 1013.25;
-            return result.ToString("0.00");
-        }
-
-        public static string mbToInHg(double input)
-        {
-            double result = 29.92 * input / 1013.25;
-            return result.ToString("0.00");
+            return Math.Round(result, 2).ToString();
         }
 
         public static string kmToMi(String input)
         {
             double result = KM_TO_MI * double.Parse(input);
-            return Math.Round(result, 2).ToString();
-        }
-
-        public static string kmToMi(double input)
-        {
-            double result = KM_TO_MI * input;
-            return Math.Round(result, 2).ToString();
+            return Math.Round(result).ToString();
         }
 
         public static string miToKm(String input)
         {
             double result = MI_TO_KM * double.Parse(input);
-            return Math.Round(result, 2).ToString();
-        }
-
-        public static string miToKm(double input)
-        {
-            double result = MI_TO_KM * input;
-            return Math.Round(result, 2).ToString();
+            return Math.Round(result).ToString();
         }
 
         public static string mphTokph(String input)
         {
             double result = MI_TO_KM * double.Parse(input);
-            return Math.Round(result).ToString();
-        }
-
-        public static string mphTokph(double input)
-        {
-            double result = MI_TO_KM * input;
             return Math.Round(result).ToString();
         }
 
@@ -65,9 +41,15 @@ namespace SimpleWeather
             return Math.Round(result).ToString();
         }
 
-        public static string kphTomph(double input)
+        public static string FtoC(String input)
         {
-            double result = KM_TO_MI * input;
+            double result = (double.Parse(input) - 32) * ((double)5 / 9);
+            return Math.Round(result).ToString();
+        }
+
+        public static string CtoF(string input)
+        {
+            double result = (double.Parse(input) * ((double)9 /5)) + 32;
             return Math.Round(result).ToString();
         }
     }
