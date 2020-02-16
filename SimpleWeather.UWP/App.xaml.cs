7c35007b2cbadb7b8dc524f89503e8e2fd17f89c
Dave Antoine <bryan2894@gmail.com>
2020-02-16 17:57:44 -0500

    App: invoke GC collection when in background and under memory pressure

--- SimpleWeather.UWP/App.xaml.cs
+++ SimpleWeather.UWP/App.xaml.cs
@@ -20,6 +20,7 @@ using Windows.ApplicationModel.Activation;
 using Windows.ApplicationModel.Core;
 using Windows.ApplicationModel.Resources;
 using Windows.Foundation.Metadata;
+using Windows.System;
 using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.ViewManagement;
@@ -93,6 +94,17 @@ namespace SimpleWeather.UWP
             this.Suspending += OnSuspending;
             this.EnteredBackground += OnEnteredBackground;
             this.LeavingBackground += OnLeavingBackground;
+            // During the transition from foreground to background the
+            // memory limit allowed for the application changes. The application
+            // has a short time to respond by bringing its memory usage
+            // under the new limit.
+            MemoryManager.AppMemoryUsageLimitChanging += MemoryManager_AppMemoryUsageLimitChanging;
+
+            // After an application is backgrounded it is expected to stay
+            // under a memory target to maintain priority to keep running.
+            // Subscribe to the event that informs the app of this change.
+            MemoryManager.AppMemoryUsageIncreased += MemoryManager_AppMemoryUsageIncreased;
+
             AppCenter.Start(APIKeys.GetAppCenterSecret(), typeof(Analytics), typeof(Crashes));
             UISettings = new UISettings();
             IsSystemDarkTheme = UISettings.GetColorValue(UIColorType.Background).ToString() == "#FF000000";
@@ -110,6 +122,103 @@ namespace SimpleWeather.UWP
                     RequestedTheme = ApplicationTheme.Dark;
                     break;
             }
+        }
+
+        /// <summary>
+        /// Handle system notifications that the app has increased its
+        /// memory usage level compared to its current target.
+        /// </summary>
+        /// <remarks>
+        /// The app may have increased its usage or the app may have moved
+        /// to the background and the system lowered the target for the app
+        /// In either case, if the application wants to maintain its priority
+        /// to avoid being suspended before other apps, it may need to reduce
+        /// its memory usage.
+        ///
+        /// This is not a replacement for handling AppMemoryUsageLimitChanging
+        /// which is critical to ensure the app immediately gets below the new
+        /// limit. However, once the app is allowed to continue running and
+        /// policy is applied, some apps may wish to continue monitoring
+        /// usage to ensure they remain below the limit.
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MemoryManager_AppMemoryUsageIncreased(object sender, object e)
+        {
+            // Obtain the current usage level
+            var level = MemoryManager.AppMemoryUsageLevel;
+
+            // Check the usage level to determine whether reducing memory is necessary.
+            // Memory usage may have been fine when initially entering the background but
+            // the app may have increased its memory usage since then and will need to trim back.
+            if (level == AppMemoryUsageLevel.OverLimit || level == AppMemoryUsageLevel.High)
+            {
+                ReduceMemoryUsage(MemoryManager.AppMemoryUsageLimit);
+            }
+        }
+
+        /// <summary>
+        /// Raised when the memory limit for the app is changing, such as when the app
+        /// enters the background.
+        /// </summary>
+        /// <remarks>
+        /// If the app is using more than the new limit, it must reduce memory within 2 seconds
+        /// on some platforms in order to avoid being suspended or terminated.
+        ///
+        /// While some platforms will allow the application
+        /// to continue running over the limit, reducing usage in the time
+        /// allotted will enable the best experience across the broadest range of devices.
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MemoryManager_AppMemoryUsageLimitChanging(object sender, AppMemoryUsageLimitChangingEventArgs e)
+        {
+            // If app memory usage is over the limit, reduce usage within 2 seconds
+            // so that the system does not suspend the app
+            if (MemoryManager.AppMemoryUsage >= e.NewLimit)
+            {
+                ReduceMemoryUsage(e.NewLimit);
+            }
+        }
+
+        /// <summary>
+        /// Reduces application memory usage.
+        /// </summary>
+        /// <remarks>
+        /// When the app enters the background, receives a memory limit changing
+        /// event, or receives a memory usage increased event, it can
+        /// can optionally unload cached data or even its view content in
+        /// order to reduce memory usage and the chance of being suspended.
+        ///
+        /// This must be called from multiple event handlers because an application may already
+        /// be in a high memory usage state when entering the background, or it
+        /// may be in a low memory usage state with no need to unload resources yet
+        /// and only enter a higher state later.
+        /// </remarks>
+        public void ReduceMemoryUsage(ulong limit)
+        {
+            // If the app has caches or other memory it can free, it should do so now.
+            // << App can release memory here >>
+
+            // Additionally, if the application is currently
+            // in background mode and still has a view with content
+            // then the view can be released to save memory and
+            // can be recreated again later when leaving the background.
+            if (IsInBackground && Window.Current?.Content != null)
+            {
+                // Some apps may wish to use this helper to explicitly disconnect
+                // child references.
+                // VisualTreeHelper.DisconnectChildrenRecursive(Window.Current.Content);
+
+                // Clear the view content. Note that views should rely on
+                // events like Page.Unloaded to further release resources.
+                // Release event handlers in views since references can
+                // prevent objects from being collected.
+                Window.Current.Content = null;
+            }
+
+            // Run the GC to collect released resources.
+            GC.Collect();
         }
 
         private void DefaultTheme_ColorValuesChanged(UISettings sender, object args)
