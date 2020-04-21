24ff45f520405facabb3217e3767cee53617e8a1
Dave Antoine <bryan2894@gmail.com>
2020-04-20 23:34:31 -0400

    SimpleWeather: add workaround for compiling System.Text.Json with the .NET Native compiler
    * Reference: https://github.com/dotnet/runtime/issues/978

--- SimpleWeather.UWP/Properties/AssemblyInfo.cs
+++ SimpleWeather.UWP/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SimpleWeather")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dave Antoine")]
 [assembly: AssemblyProduct("SimpleWeather")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.2.0.0")]
 [assembly: ComVisible(false)]
