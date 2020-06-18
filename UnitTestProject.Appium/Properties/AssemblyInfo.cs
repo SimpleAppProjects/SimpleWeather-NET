8f2cd4d8488f84391987eb2a86bda81c49f50d81
Dave Antoine <bryan2894@gmail.com>
2020-06-18 00:12:50 -0400

    Tests: add project for Appium tests

---
+++ UnitTestProject.Appium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject.Appium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject.Appium")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3e30595-b715-4be1-a166-7a67fa6dad00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject.Appium/UnitTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace UnitTestProject.Appium
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void LaunchTest_LayoutCycleException()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                // Launch the app
+                AppiumOptions opt = new AppiumOptions();
+                opt.AddAdditionalCapability("app", "49586DaveAntoine.SimpleWeatherDebug_9bzempp7dntjg!App");
+                //opt.AddAdditionalCapability("app", "49586DaveAntoine.SimpleWeather-Asimpleweatherapp_9bzempp7dntjg!App");
+                opt.AddAdditionalCapability("deviceName", "WindowsPC");
+
+                WindowsDriver<WindowsElement> session;
+
+                try
+                {
+                    session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), opt);
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        session = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), opt);
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
+
+                session.LaunchApp();
+
+                //session.FindElementByAccessibilityId("CommandBar").Click();
+
+                session.Manage().Window.Size = new System.Drawing.Size(480, 720);
+                session.Manage().Window.Maximize();
+                session.Manage().Window.Size = new System.Drawing.Size(480, 720);
+                session.Manage().Window.Maximize();
+
+                Task.Delay(1000);
+
+                session.CloseApp();
+
+                Task.Delay(1000);
+            }
+        }
+    }
+}
