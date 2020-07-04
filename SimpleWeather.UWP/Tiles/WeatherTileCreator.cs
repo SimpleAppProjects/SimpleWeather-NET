c1f91a48bc7124764cffd6ee8aab29d1e1cb76ff
Dave Antoine <bryan2894@gmail.com>
2020-07-04 01:06:47 -0400

    Tiles: handle invalid values for temperature on lockscreen

--- SimpleWeather.UWP/Tiles/WeatherTileCreator.cs
+++ SimpleWeather.UWP/Tiles/WeatherTileCreator.cs
@@ -898,7 +898,7 @@ namespace SimpleWeather.UWP.Tiles
                     },
                     LockDetailedStatus1 = weather.Location?.Ellipsize(40),
                     LockDetailedStatus2 = String.Format("{0} - {1}", weather.CurTemp?.RemoveNonDigitChars() + "º", weather.CurCondition?.Ellipsize(33)),
-                    LockDetailedStatus3 = String.Format("{0} | {1}", weather.HiTemp, weather.LoTemp)
+                    LockDetailedStatus3 = String.Format("{0} | {1}", weather.HiTemp ?? "--", weather.LoTemp ?? "--")
                 }
             };
 
