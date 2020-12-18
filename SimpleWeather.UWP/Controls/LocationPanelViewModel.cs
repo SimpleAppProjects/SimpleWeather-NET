c23bd15ae72c236a906b7afe37b73b8491dd5175
Dave Antoine <bryan2894@gmail.com>
2020-12-17 19:34:15 -0500

    LocationPanel: hide wind speed if unavailable

--- SimpleWeather.UWP/Controls/LocationPanelViewModel.cs
+++ SimpleWeather.UWP/Controls/LocationPanelViewModel.cs
@@ -218,7 +218,7 @@ namespace SimpleWeather.Controls
             }
             else
             {
-                WindSpeed = "--";
+                WindSpeed = null;
                 WindDirection = 0;
             }
         }
