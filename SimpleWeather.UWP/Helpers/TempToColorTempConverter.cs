d473140a51465791f1f3d568fbd7e7005904bdf1
Dave Antoine <bryan2894@gmail.com>
2021-02-12 13:06:47 -0500

    WeatherNow: UI fixes

--- SimpleWeather.UWP/Helpers/TempToColorTempConverter.cs
+++ SimpleWeather.UWP/Helpers/TempToColorTempConverter.cs
@@ -1,14 +1,35 @@
 ﻿using SimpleWeather.Utils;
 using System;
 using Windows.UI;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
 namespace SimpleWeather.UWP.Helpers
 {
-    public class TempToColorTempConverter : IValueConverter
+    public class TempToColorTempConverter : DependencyObject, IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
+        public Color FallbackColor
+        {
+            get { return (Color)GetValue(FallbackColorProperty); }
+            set { SetValue(FallbackColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FallbackColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FallbackColorProperty =
+            DependencyProperty.Register("FallbackColor", typeof(Color), typeof(TempToColorTempConverter), new PropertyMetadata(Colors.White));
+
+        public bool UseFallback
+        {
+            get { return (bool)GetValue(UseFallbackProperty); }
+            set { SetValue(UseFallbackProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UseFallback.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UseFallbackProperty =
+            DependencyProperty.Register("UseFallback", typeof(bool), typeof(TempToColorTempConverter), new PropertyMetadata(true));
+
+        public object Convert(object value, Type targetType, object p_, string l_)
         {
             string temp = value?.ToString();
             string temp_str = temp?.RemoveNonDigitChars();
@@ -22,30 +43,17 @@ namespace SimpleWeather.UWP.Helpers
                     temp_f = ConversionMethods.CtoF(temp_f);
                 }
 
-                Color defaultColor = Colors.White;
-                if (parameter is Color paramColor)
-                {
-                    defaultColor = paramColor;
-                }
-
-                return WeatherUtils.GetColorFromTempF(temp_f, defaultColor);
+                return WeatherUtils.GetColorFromTempF(temp_f, UseFallback ? FallbackColor : Colors.White);
             }
             else
             {
-                if (parameter is Color defaultColor)
-                {
-                    return defaultColor;
-                }
-                else
-                {
-                    return Colors.White;
-                }
+                return UseFallback ? FallbackColor : Colors.White;
             }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
-            return null;
+            throw new NotImplementedException();
         }
     }
 }
