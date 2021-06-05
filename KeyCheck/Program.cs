3a3325b2c75f7035805d45f19454b63626cb8e08
Dave Antoine <bryan2894@gmail.com>
2021-06-05 14:28:51 -0400

    KeyCheck: update program

--- KeyCheck/Program.cs
+++ KeyCheck/Program.cs
@@ -12,47 +12,57 @@ namespace KeyCheck
     {
         private static void Main(string[] args)
         {
-            if (args == null || args.Length < 1)
+            if (args == null || args.Length != 1)
             {
                 Console.WriteLine("Missing arguments!!");
                 Environment.ExitCode = 1;
             }
             else
             {
-                foreach (var FilePath in args)
+                var DirectoryPath = args[0];
+
+                if (!Directory.Exists(DirectoryPath))
                 {
-                    try
+                    Console.WriteLine("Directory \"{0}\" does not exist!!", DirectoryPath);
+                    Environment.ExitCode = 1;
+                }
+                else
+                {
+                    var KeyFiles = Directory.EnumerateFiles(DirectoryPath, "*.cs", SearchOption.TopDirectoryOnly);
+
+                    foreach (var FilePath in KeyFiles)
                     {
-                        Console.WriteLine("File path: {0}", FilePath);
-                        var content = File.ReadAllText(FilePath);
-                        if (content.Contains("return null;") ||
-                            !Regex.Match(content, "return \".*\";", RegexOptions.IgnoreCase).Success)
+                        try
                         {
-                            Console.WriteLine("Key missing from file!!");
+                            Console.WriteLine("File path: {0}", FilePath);
+                            var content = File.ReadAllText(FilePath);
+                            if (content.Contains("return null;") ||
+                                !Regex.Match(content, "return \".*\";", RegexOptions.IgnoreCase).Success)
+                            {
+                                Console.WriteLine("Key missing from file!!");
+                                Environment.ExitCode = 1;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Key found!!");
+                            }
+                        }
+                        catch (FileNotFoundException)
+                        {
+                            Console.WriteLine("File \"{0}\" does not exist!!", FilePath);
                             Environment.ExitCode = 1;
                         }
-                        else
+                        catch (Exception)
                         {
-                            Console.WriteLine("Key found!!");
+                            Console.WriteLine("Error accessing file!!");
+                            Environment.ExitCode = 1;
                         }
-                    }
-                    catch (FileNotFoundException)
-                    {
-                        Console.WriteLine("File \"{0}\" does not exist!!", FilePath);
-                        Environment.ExitCode = 1;
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Error accessing file!!");
-                        Environment.ExitCode = 1;
-                    }
-                    finally
-                    {
-                        if (Environment.ExitCode != 0)
-                            Environment.Exit(Environment.ExitCode);
                     }
                 }
             }
+
+            if (Environment.ExitCode != 0)
+                Environment.Exit(Environment.ExitCode);
         }
     }
 }
