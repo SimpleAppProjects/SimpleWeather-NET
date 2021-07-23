ad1c8b6a5a59b7b729b7e237947f19fa6f662328
Dave Antoine <bryan2894@gmail.com>
2021-07-22 20:23:05 -0400

    Helpers: add StringRes markup extension
    * To help cleanup string resources and avoid unecessary duplicates

---
+++ SimpleWeather.UWP/Helpers/StringRes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.UI.Xaml.Markup;
+
+namespace SimpleWeather.UWP.Helpers
+{
+    [MarkupExtensionReturnType(ReturnType = typeof(string))]
+    public class StringRes : MarkupExtension
+    {
+        public string Name { get; set; }
+
+        protected override object ProvideValue()
+        {
+            return GetResourceLoader().GetString(Name);
+        }
+
+        private ResourceLoader GetResourceLoader()
+        {
+            if (App.ResLoader != null)
+            {
+                return App.ResLoader;
+            }
+
+            if (Windows.UI.Core.CoreWindow.GetForCurrentThread() != null)
+            {
+                return ResourceLoader.GetForCurrentView();
+            }
+            else
+            {
+                return ResourceLoader.GetForViewIndependentUse();
+            }
+        }
+    }
+}
