3ba9918d4f5a8cfd22e3e8c5c70b344170ee03c9
Dave Antoine <bryan2894@gmail.com>
2021-08-08 14:27:46 -0400

    ForecastsNowViewModel: MinutelyPrecipitationGraphData is nullable

--- SimpleWeather.UWP/Controls/ViewModels/ForecastsNowViewModel.cs
+++ SimpleWeather.UWP/Controls/ViewModels/ForecastsNowViewModel.cs
@@ -94,7 +94,7 @@ namespace SimpleWeather.UWP.Controls
         {
             if (dp == HourlyPrecipitationGraphDataProperty || dp == MinutelyPrecipitationGraphDataProperty)
             {
-                IsPrecipitationDataPresent = !(HourlyPrecipitationGraphData?.IsEmpty == true && MinutelyPrecipitationGraphData.IsEmpty == true);
+                IsPrecipitationDataPresent = !(HourlyPrecipitationGraphData?.IsEmpty == true && MinutelyPrecipitationGraphData?.IsEmpty == true);
             }
         }
 
