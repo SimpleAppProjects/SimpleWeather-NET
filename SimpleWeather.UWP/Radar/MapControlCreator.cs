71f9c5ebef0dca264f595f9a3559fcc19ca3eecb
Dave Antoine <thewizrd.dev@gmail.com>
2022-10-15 21:40:21 -0400

    MapControlCreator: null check

--- SimpleWeather.UWP/Radar/MapControlCreator.cs
+++ SimpleWeather.UWP/Radar/MapControlCreator.cs
@@ -1,9 +1,4 @@
 ﻿using SimpleWeather.Keys;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls.Maps;
 
 namespace SimpleWeather.UWP.Radar
@@ -36,7 +31,7 @@ namespace SimpleWeather.UWP.Radar
 
         public void RemoveMapControl()
         {
-            _mapControl.TileSources.Clear();
+            _mapControl?.TileSources?.Clear();
             _mapControl = null;
         }
     }
