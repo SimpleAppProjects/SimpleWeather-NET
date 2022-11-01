1c251d6ed1963ead836774444b398129a1afe545
Dave Antoine <thewizrd.dev@gmail.com>
2022-10-31 22:44:48 -0400

    WeatherNow: fix loading state binding

--- SimpleWeather.UWP/DataBinding/WeatherNowBinding.cs
+++ SimpleWeather.UWP/DataBinding/WeatherNowBinding.cs
@@ -1,17 +1,18 @@
 ﻿using SimpleWeather.Location;
+using SimpleWeather.ViewModels;
 using Windows.UI.Xaml;
 
 namespace SimpleWeather.UWP.DataBinding
 {
     public static class WeatherNowBinding
     {
-        public static bool IsLoadingRingActive(bool isLoading, bool noLocationAvailable, string locationName)
+        public static bool IsLoadingRingActive(WeatherNowState state)
         {
-            return (isLoading && (noLocationAvailable || string.IsNullOrWhiteSpace(locationName))) || isLoading;
+            return (state.IsLoading && (state.NoLocationAvailable || string.IsNullOrWhiteSpace(state.Weather?.Location))) || state.IsImageLoading;
         }
-        public static Visibility IsViewVisible(bool isLoading, bool noLocationAvailable, string locationName)
+        public static Visibility IsViewVisible(WeatherNowState state)
         {
-            return !IsLoadingRingActive(isLoading, noLocationAvailable, locationName) ? Visibility.Visible : Visibility.Collapsed;
+            return (state.IsLoading && (state.NoLocationAvailable || string.IsNullOrWhiteSpace(state.Weather?.Location))) ? Visibility.Collapsed : Visibility.Visible;
         }
         public static Visibility ShowGPSIcon(LocationType locationType)
         {
