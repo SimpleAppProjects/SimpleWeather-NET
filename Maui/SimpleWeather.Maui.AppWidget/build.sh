db7155b2010b301c98cd122e97cea8db43f977b2
Dave Antoine <thewizrd.dev@gmail.com>
2023-10-15 03:04:53 -0400

    AppWidget: update scripts

--- Maui/SimpleWeather.Maui.AppWidget/build.sh
+++ Maui/SimpleWeather.Maui.AppWidget/build.sh
@@ -3,16 +3,10 @@
 xcodebuild clean
 rm -rf DerivedData
 
-xcodebuild build -configuration Debug -target widgetExtension -sdk iphonesimulator
-xcodebuild build -configuration Debug -target widgetExtension -sdk iphoneos
-xcodebuild build -configuration Debug -target widgetExtension -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES
-xcodebuild build -configuration Debug -target widgetintentExt -sdk iphonesimulator
-xcodebuild build -configuration Debug -target widgetintentExt -sdk iphoneos
-xcodebuild build -configuration Debug -target widgetintentExt -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES
+xcodebuild build -configuration Debug -scheme widgetExtension -sdk iphonesimulator
+xcodebuild build -configuration Debug -scheme widgetExtension -sdk iphoneos
+xcodebuild build -configuration Debug -scheme widgetExtension -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES
 
-xcodebuild build -configuration Release -target widgetExtension -sdk iphonesimulator
-xcodebuild build -configuration Release -target widgetExtension -sdk iphoneos
-xcodebuild build -configuration Release -target widgetExtension -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES
-xcodebuild build -configuration Release -target widgetintentExt -sdk iphonesimulator
-xcodebuild build -configuration Release -target widgetintentExt -sdk iphoneos
-xcodebuild build -configuration Release -target widgetintentExt -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES
+xcodebuild build -configuration Release -scheme widgetExtension -sdk iphonesimulator
+xcodebuild build -configuration Release -scheme widgetExtension -sdk iphoneos
+xcodebuild build -configuration Release -scheme widgetExtension -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES
