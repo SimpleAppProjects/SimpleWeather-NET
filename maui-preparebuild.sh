2e8664245c1a34e112c6957db9266fd534589b28
Dave Antoine <thewizrd.dev@gmail.com>
2023-10-22 00:28:23 -0400

    Maui.AppWidget: update project schemes

--- maui-preparebuild.sh
+++ maui-preparebuild.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 config=$1
+cleanWidgets=$2
 
 KeyCheck/bin/Release/net7.0/publish/KeyCheck SimpleWeather.Shared/Keys
 KeyCheck/bin/Release/net7.0/publish/KeyCheck SimpleWeather.Weather-API/Keys
 
 cd Maui/SimpleWeather.Maui.AppWidget
 
-xcodebuild build -configuration $config -scheme widgetExtension -sdk iphonesimulator
-xcodebuild build -configuration $config -scheme widgetExtension -sdk iphoneos
-xcodebuild build -configuration $config -scheme widgetExtension -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES SKIP_INSTALL=NO
+if [ "$cleanWidgets" = "clean"  ]; then
+    xcodebuild clean; rm -rf DerivedData
+fi
+
+xcodebuild build -configuration $config -scheme "SimpleWeather-Extensions" -sdk iphonesimulator
+xcodebuild build -configuration $config -scheme "SimpleWeather-Extensions" -sdk iphoneos
+xcodebuild build -configuration $config -scheme "SimpleWeather-Extensions" -sdk macosx -destination 'platform=macOS,variant=Mac Catalyst' SUPPORTS_MACCATALYST=YES SKIP_INSTALL=NO
 
 cd ../SimpleWeather.Maui
 
