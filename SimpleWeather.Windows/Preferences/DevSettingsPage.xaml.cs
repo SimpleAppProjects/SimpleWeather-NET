6ca26eb4bceb4f120b67f6e1d0251232be3828ff
Dave Antoine <thewizrd.dev@gmail.com>
2023-12-16 19:55:43 -0500

    DevSettings: add support to extract logs

--- SimpleWeather.Windows/Preferences/DevSettingsPage.xaml.cs
+++ SimpleWeather.Windows/Preferences/DevSettingsPage.xaml.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using CommunityToolkit.WinUI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using SimpleWeather.NET.Controls;
 using SimpleWeather.Utils;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using ResStrings = SimpleWeather.Resources.Strings.Resources;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,6 +29,8 @@ namespace SimpleWeather.NET.Preferences
             DebugSwitch.IsEnabled = true;
 #endif
             DebugSwitch.Toggled += DebugSwitch_Toggled;
+
+            SaveLogsBtn.Click += SaveLogsBtn_Click;
         }
 
         private void DebugSwitch_Toggled(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
@@ -30,6 +38,102 @@ namespace SimpleWeather.NET.Preferences
             var sw = sender as ToggleSwitch;
 
             Logger.EnableDebugLogger(sw.IsOn);
+        }
+
+        private async void SaveLogsBtn_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            void ShowNoLogsFoundPrompt()
+            {
+                if (this.Content is Panel root)
+                {
+                    DispatcherQueue.EnqueueAsync(() =>
+                    {
+                        var snackMgr = new SnackbarManager(root);
+                        snackMgr.Show(Snackbar.Make("No logs found", SnackbarDuration.Short));
+                    });
+                }
+            }
+
+            try
+            {
+                var logsDirectory = await ApplicationData.Current.LocalFolder.GetFolderAsync("logs");
+                var logFiles = await logsDirectory.GetFilesAsync();
+
+                if (logFiles.Count == 0)
+                {
+                    ShowNoLogsFoundPrompt();
+                    return;
+                }
+
+                await DispatcherQueue.EnqueueAsync(async () =>
+                {
+                    var contentDialog = new ContentDialog
+                    {
+                        IsPrimaryButtonEnabled = false,
+                        IsSecondaryButtonEnabled = true,
+                        Title = "Save Logs",
+                        XamlRoot = this.XamlRoot,
+                        SecondaryButtonText = ResStrings.Label_Cancel
+                    };
+
+                    contentDialog.SecondaryButtonCommand = new RelayCommand(() =>
+                    {
+                        contentDialog.Hide();
+                    });
+
+                    var listView = new ListView()
+                    {
+                        IsItemClickEnabled = true,
+                        IsSwipeEnabled = false,
+                        IsMultiSelectCheckBoxEnabled = false,
+                        ItemTemplate = this.Resources["LogFileTemplate"] as DataTemplate,
+                        ItemsSource = logFiles
+                    };
+
+                    listView.ItemClick += async (s, e) =>
+                    {
+                        if (e.ClickedItem is not StorageFile logFile) return;
+
+                        try
+                        {
+                            // Create a file picker
+                            var savePicker = new FileSavePicker();
+
+                            // Retrieve the window handle (HWND) of the current WinUI 3 window.
+                            var window = MainWindow.Current;
+                            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+                            // Initialize the file picker with the window handle (HWND).
+                            WinRT.Interop.InitializeWithWindow.Initialize(savePicker, hWnd);
+
+                            // Set options for your file picker
+                            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+                            // Dropdown of file types the user can save the file as
+                            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".log" });
+                            savePicker.SuggestedFileName = logFile.Name;
+
+                            // Open the picker for the user to pick a file
+                            var file = await savePicker.PickSaveFileAsync();
+
+                            if (file != null)
+                            {
+                                await logFile.CopyAndReplaceAsync(file);
+                                contentDialog.Hide();
+                            }
+                        }
+                        catch { }
+                    };
+
+                    contentDialog.Content = listView;
+
+                    await contentDialog.ShowAsync();
+                });
+            }
+            catch (FileNotFoundException)
+            {
+                // Ignore error
+                ShowNoLogsFoundPrompt();
+            }
         }
     }
 }
