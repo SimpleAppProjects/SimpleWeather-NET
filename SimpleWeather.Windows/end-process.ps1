7d97607020b97064a3981acd777056eb854331c6
Dave Antoine <thewizrd.dev@gmail.com>
2024-10-27 19:23:38 -0400

    SimpleWeather.Windows: v5.10.0

--- SimpleWeather.Windows/end-process.ps1
+++ SimpleWeather.Windows/end-process.ps1
@@ -2,3 +2,4 @@ $WorkingDir = pwd
 $Condition = "${WorkingDir}\*SimpleWeather.Windows.exe*"
 
 Get-Process | Where-Object {$_.Path -like $Condition} | Stop-Process -Force
+Get-Process -Name backgroundTaskHost | Where-Object {$_.Modules -like "*Debug*SimpleWeather*"} | Stop-Process -Force
